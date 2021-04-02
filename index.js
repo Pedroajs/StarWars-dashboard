4d264ade12b26ac48e1f1f7958d148c2ef16055b
Pedro Augusto <54971305+Pedroajs@users.noreply.github.com>
2021-04-02 18:34:50 -0300

    request a resolver

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
-const url = "https://swapi.dev/api/people/1/";
 
-fetch(url)
-    .then(response => response.json())
-    .then(json => console.log(json))
+const personagens = document.getElementById("personagens");
+const luas = document.getElementById("luas");
+const planetas = document.getElementById("planetas");
+const naves = document.getElementById("naves");
+
+function preencherAssets(){
+    personagens.innerHTML = swapiGet("people/1/");
+    console.log('primeiro')
+
+
+}
+
+function swapiGet(params){
+    const url = `https://swapi.dev/api/${params}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(json => json)
+
+}
+preencherAssets();
